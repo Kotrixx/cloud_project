b7cc40bfb360a2e187cdd85ab1657efe83181b11
mathias-is-online <a20203607@pucp.edu.pe>
2024-10-12 19:23:53 -0500

    slice manager

---
+++ app/utils/llenar_headnode_new.py
@@ -0,0 +1,127 @@
+import subprocess
+import json
+import ipaddress
+
+# Función para ejecutar comandos con sudo
+def ejecutar_comando_sudo(comando, descripcion=""):
+    if descripcion:
+        print(descripcion)
+    comando_con_sudo = f"echo 'ubuntu' | sudo -S {comando}"
+    proceso = subprocess.Popen(comando_con_sudo, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    salida, error = proceso.communicate()
+    if salida:
+        print(salida)
+    if error:
+        print(f"Error: {error}")
+
+# Cargar la configuración JSON
+def cargar_configuracion(json_file):
+    with open(json_file, 'r') as f:
+        config = json.load(f)
+    return config
+
+# Función para obtener la IP inmediata superior en una subred
+def obtener_ip_superior(subred, offset):
+    network = ipaddress.ip_network(subred, strict=False)
+    return str(network.network_address + offset)
+
+# Función principal para configurar el HeadNode de forma dinámica
+def configurar_headnode(config):
+    print("Configuración iniciada en HeadNode")
+
+    # Actualizar el sistema
+    ejecutar_comando_sudo('apt-get update', "Actualizando el sistema... (esto puede tardar, no detener el programa)")
+
+    # Crear un nuevo OVS llamado br-int
+    ejecutar_comando_sudo('ovs-vsctl add-br br-int', "Creando el bridge OVS br-int...")
+    ejecutar_comando_sudo('ip link set dev br-int up', "Levantando el bridge br-int...")
+
+    # Prender interfaz ens5 y agregarla al OVS
+    ejecutar_comando_sudo('ip link set ens5 up', "Levantando la interfaz ens5...")
+    ejecutar_comando_sudo('ovs-vsctl add-port br-int ens5', "Conectando la interfaz ens5 al OVS...")
+
+    # Activar IPv4 Forwarding
+    ejecutar_comando_sudo('sysctl -w net.ipv4.ip_forward=1', "Activando IPv4 Forwarding...")
+
+    # Procesar cada VLAN
+    vlans = config['vlans']
+    veth_pairs = []
+    subinterfaces = {}
+    gateway_ips = {}
+    veth_ips = {}
+    dnsmasq_configs = {}
+    vlan_tags = {}
+
+    for vlan_tag, red in vlans.items():
+        ns = f'ns-vlan{vlan_tag}'
+        veth_ovs = f'veth-ovs{vlan_tag}'
+        veth_ns = f'veth-ns{vlan_tag}'
+        
+        # Crear namespace y levantarlo
+        ejecutar_comando_sudo(f'ip netns add {ns}', f"Creando el namespace {ns}...")
+        ejecutar_comando_sudo(f'ip netns exec {ns} ip link set dev lo up', f"Levantando la interfaz loopback en {ns}...")
+
+        # Añadir el par de veths
+        veth_pairs.append((veth_ovs, veth_ns, ns))
+
+        # Crear subinterface y asignarle la VLAN
+        subinterface = f'br-int.{vlan_tag}'
+        subinterfaces[subinterface] = vlan_tag
+
+        # Asignar las IPs (primera IP superior es gateway, segunda IP superior es para veth)
+        gateway_ip = obtener_ip_superior(red, 1) + '/' + red.split('/')[1]  # IP inmediatamente superior a la red es gateway
+        veth_ip = obtener_ip_superior(red, 2) + '/' + red.split('/')[1]  # Segunda IP es para veth en el namespace
+        gateway_ips[subinterface] = gateway_ip
+        veth_ips[ns] = veth_ip
+
+        # Configuración de dnsmasq para cada namespace (IPs en bloques de 4)
+        dnsmasq_range_start = obtener_ip_superior(red, 3)  # La tercera IP superior
+        dnsmasq_range_end = obtener_ip_superior(red, 6)    # Usar las siguientes 4 direcciones
+        dnsmasq_configs[ns] = (veth_ns, f"{dnsmasq_range_start},{dnsmasq_range_end}", gateway_ip)
+
+        # Asignar tag de VLAN al veth del OVS
+        vlan_tags[veth_ovs] = vlan_tag
+
+    # Crear y configurar veth pairs
+    for veth_ovs, veth_ns, ns in veth_pairs:
+        ejecutar_comando_sudo(f'ip link add {veth_ovs} type veth peer name {veth_ns}', f"Creando el par veth {veth_ovs} y {veth_ns}...")
+        ejecutar_comando_sudo(f'ip link set {veth_ns} netns {ns}', f"Moviendo {veth_ns} al namespace {ns}...")
+        ejecutar_comando_sudo(f'ovs-vsctl add-port br-int {veth_ovs}', f"Añadiendo {veth_ovs} al OVS br-int...")
+        ejecutar_comando_sudo(f'ip link set {veth_ovs} up', f"Levantando {veth_ovs}...")
+        ejecutar_comando_sudo(f'ip netns exec {ns} ip link set dev {veth_ns} up', f"Levantando {veth_ns} en {ns}...")
+
+    # Crear subinterfaces en br-int y asignar VLANs
+    for subinterface, vlan in subinterfaces.items():
+        ejecutar_comando_sudo(f'ip link add link br-int name {subinterface} type vlan id {vlan}', f"Creando subinterface {subinterface} con VLAN {vlan}...")
+        ejecutar_comando_sudo(f'ip link set dev {subinterface} up', f"Levantando la subinterface {subinterface}...")
+
+    # Asignar IPs a las subinterfaces para que actúen como gateways
+    for subinterface, ip in gateway_ips.items():
+        ejecutar_comando_sudo(f'ip address add {ip} dev {subinterface}', f"Asignando IP {ip} a {subinterface}...")
+
+    # Asignar IPs a las interfaces veth del namespace
+    for ns, ip in veth_ips.items():
+        ejecutar_comando_sudo(f'ip netns exec {ns} ip address add {ip} dev veth-ns{ns[-3:]}', f"Asignando IP {ip} a {ns}...")
+
+    # Ejecutar dnsmasq en cada namespace para proporcionar DHCP
+    for ns, (iface, dhcp_range, gateway) in dnsmasq_configs.items():
+        ejecutar_comando_sudo(f'ip netns exec {ns} dnsmasq --interface={iface} --dhcp-range={dhcp_range},255.255.255.248 --dhcp-option=3,{gateway} --dhcp-option=6,8.8.8.8', f"Configurando dnsmasq en {ns}...")
+
+    # Asignar tag de VLAN a interfaces del peer veth del OVS
+    for veth_ovs, vlan in vlan_tags.items():
+        ejecutar_comando_sudo(f'ovs-vsctl set port {veth_ovs} tag={vlan}', f"Asignando tag VLAN {vlan} a {veth_ovs}...")
+
+    # Definir la interfaz ens5 como troncal para que todas las VLANs puedan llevar y recoger tráfico
+    vlan_ids = ",".join([str(vlan) for vlan in vlans.keys()])
+    ejecutar_comando_sudo(f'ovs-vsctl set port ens5 trunk={vlan_ids}', f"Configurando ens5 como troncal para las VLANs {vlan_ids}...")
+
+    # Natear las redes definidas en el archivo JSON
+    for red in vlans.values():
+        ejecutar_comando_sudo(f'iptables -t nat -I POSTROUTING -s {red} -o ens3 -j MASQUERADE', f"Aplicando regla NAT para {red}...")
+
+    print("Configuración completa en HeadNode")
+
+# Cargar el archivo de configuración y ejecutar la configuración
+json_file = "workers_config.json"
+config = cargar_configuracion(json_file)
+configurar_headnode(config)
---
+++ app/utils/llenar_worker_new.py
@@ -0,0 +1,90 @@
+# Función para ejecutar los comandos en el worker
+def ejecutar_comandos_worker(cliente, worker_config):
+    bridge = worker_config['bridge']
+    vms = worker_config['vms']
+    
+    # Crear el bridge y levantarlo
+    comandos = [
+        (f"ovs-vsctl add-br {bridge}", f"Creando el bridge OVS {bridge}..."),
+        (f"ip link set dev {bridge} up", f"Levantando el bridge {bridge}...")
+    ]
+    
+    # Procesar cada VM
+    for vm_name, vm_config in vms.items():
+        interfaces = vm_config['interfaces']
+        
+        # Crear las interfaces TAP para cada VM y configurarlas
+        for interfaz in interfaces:
+            tap_name = interfaz['nombre']
+            mac_address = interfaz['mac']
+            vlan_tag = interfaz['vlan']
+            
+            comandos += [
+                (f"ip tuntap add mode tap name {tap_name}", f"Creando interfaz {tap_name}..."),
+                (f"ip link set {tap_name} up", f"Levantando {tap_name}..."),
+                (f"ovs-vsctl add-port {bridge} {tap_name}", f"Conectando {tap_name} al bridge..."),
+                (f"ovs-vsctl set port {tap_name} tag={vlan_tag}", f"Asignando tag VLAN {vlan_tag} a {tap_name}..."),
+                (f"ip link set {tap_name} address {mac_address}", f"Asignando MAC {mac_address} a {tap_name}...")
+            ]
+        
+        # Crear comando dinámico de QEMU según el número de interfaces
+        comando_qemu = f"qemu-system-x86_64 -enable-kvm -vnc 0.0.0.0:{vm_name[-1]} "  # Usa el número de VM para VNC
+        for i, interfaz in enumerate(interfaces):
+            tap_name = interfaz['nombre']
+            mac_address = interfaz['mac']
+            comando_qemu += f"-netdev tap,id={tap_name},ifname={tap_name},script=no,downscript=no -device e1000,netdev={tap_name},mac={mac_address} "
+
+        # Completar comando con opciones adicionales
+        comando_qemu += "-daemonize -snapshot cirros-0.5.1-x86_64-disk.img -cpu host"
+        comandos.append((comando_qemu, f"Creando VM {vm_name} con {len(interfaces)} interfaces..."))
+
+    # Ejecutar todos los comandos
+    for comando, descripcion in comandos:
+        ejecutar_comando_sudo(cliente, comando, descripcion)
+
+# Función para conectarse al worker mediante SSH
+def conectar_worker(ip, usuario, contrasena):
+    print(f"Conectando al worker {ip}...")
+    cliente = paramiko.SSHClient()
+    cliente.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    cliente.connect(ip, username=usuario, password=contrasena)
+    print(f"Conexión establecida con el worker {ip}")
+    return cliente
+
+# Función para ejecutar comandos con sudo y manejar la contraseña
+def ejecutar_comando_sudo(cliente, comando, descripcion=""):
+    if descripcion:
+        print(descripcion)
+    comando_con_sudo = f"echo 'ubuntu' | sudo -S {comando}"
+    stdin, stdout, stderr = cliente.exec_command(comando_con_sudo)
+    salida = stdout.read().decode()
+    error = stderr.read().decode()
+    if salida:
+        print(salida)
+    if error:
+        print(f"Error: {error}")
+
+# Cargar la configuración JSON
+def cargar_configuracion(json_file):
+    with open(json_file, 'r') as f:
+        config = json.load(f)
+    return config
+
+# Función principal para procesar los workers y ejecutar los comandos en cada uno
+def procesar_workers(config, usuario, contrasena):
+    workers = config['workers']
+    
+    for worker_name, worker_config in workers.items():
+        ip = worker_config['ip']
+        cliente = conectar_worker(ip, usuario, contrasena)
+        ejecutar_comandos_worker(cliente, worker_config)
+        cliente.close()
+
+# Configuración de la ruta del archivo JSON y credenciales SSH
+json_file = "workers_config.json"
+usuario = "ubuntu"
+contrasena = "ubuntu"
+
+# Cargar la configuración y procesar los workers
+config = cargar_configuracion(json_file)
+procesar_workers(config, usuario, contrasena)
